342aeee599bfe7300d44b2a950cfbab6707ea561
Shahriyar <mdshahriyaralam9@gmail.com>
2022-07-18 04:00:27 +0600

    Added ProfileLayout

---
+++ src/components/Layouts/ProfilePage.tsx
@@ -0,0 +1,29 @@
+import SidebarLink from '@components/SidebarLink/SidebarLink'
+import Header from '@sections/Header/Header'
+import Sidebar from '@sections/Sidebar/Sidebar'
+import React from 'react'
+
+type Props = {
+  children: React.ReactNode | React.ReactNode[]
+}
+
+const ProfilePage = ({ children }: Props) => {
+  return (
+    <>
+      <Header />
+      <div className='container mx-auto grid grid-cols-1 sm:grid-cols-sidebarLayout gap-5 px-5 sm:px-7 md:px-10 mt-3 sm:mt-5 md:mt-10'>
+        <Sidebar>
+          <div className='flex flex-col gap-2'>
+            <SidebarLink href='/profile/posts'>Posts</SidebarLink>
+            <SidebarLink href='/profile/posts/create'>New Post</SidebarLink>
+          </div>
+        </Sidebar>
+        <div className='pb-10 min-h-[90vh]'>
+          {children}
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default ProfilePage
