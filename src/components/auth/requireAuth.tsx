cc5ddac65dfe8ddd2164257f9bad39125d876b4a
Shahriyar <mdshahriyaralam9@gmail.com>
2022-07-18 03:59:45 +0600

    Added requireauth

---
+++ src/components/auth/requireAuth.tsx
@@ -0,0 +1,30 @@
+import ProfilePage from "@layouts/ProfilePage";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import React from "react";
+import { Children } from "../../types/common";
+
+type Props = {
+  children: Children 
+}
+
+const RequireAuth = ({ children }: Props) => {
+  const { status } = useSession();
+  const router = useRouter();
+
+  if (status === "loading") {
+    return (
+      <ProfilePage>
+        <p>Loading...</p>
+      </ProfilePage>
+    )
+  }
+
+  if (status === "unauthenticated") {
+    router.push("/");
+  }
+
+  return children as JSX.Element;
+};
+
+export default RequireAuth;
