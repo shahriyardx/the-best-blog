d0826a8ad97bf6d8e8c5f0f19f9cb0919e486794
Shahriyar <mdshahriyaralam9@gmail.com>
2022-07-19 00:16:43 +0600

    Dark mode header

--- components/sections/Header/DashHeader.tsx
+++ components/sections/Header/DashHeader.tsx
@@ -8,14 +8,14 @@ const DashHeader = () => {
   const { toggle } = useContext(SidebarContext)
 
   return (
-    <header className='bg-zinc-800'>
+    <header className='bg-orange-200 dark:bg-zinc-800'>
       <div className='container mx-auto px-5 sm:px-7 md:px-10 py-3 flex items-center'>
         <BiMenu
           onClick={toggle}
           className='text-3xl mr-3 cursor-pointer sm:hidden -mb-1 dark:text-zinc-300' 
         />
         <Link href="/admin" passHref>
-          <a className='text-2xl font-semibold dark:text-zinc-300'>Dashboard</a>
+          <a className='text-2xl font-semibold text-zinc-800 dark:text-zinc-300'>Dashboard</a>
         </Link>
 
         <ThemeToggle />
