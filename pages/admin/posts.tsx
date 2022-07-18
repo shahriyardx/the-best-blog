0a0bbe713a295b79e77e6dedc41899a6151ead07
Shahriyar <mdshahriyaralam9@gmail.com>
2022-07-18 20:35:02 +0600

    Added posts page

---
+++ pages/admin/posts.tsx
@@ -0,0 +1,12 @@
+import AdminPage from "components/Layouts/AdminPage";
+import { NextPage } from "next";
+
+const Posts: NextPage = () => {
+  return (
+    <AdminPage>
+      <span>WTF</span>
+    </AdminPage>
+  )
+}
+
+export default Posts
