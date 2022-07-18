af9942edf3ae045827e135392cd862b6c436352e
Shahriyar <mdshahriyaralam9@gmail.com>
2022-07-18 11:59:58 +0600

    Added about and conatct page

---
+++ src/pages/about.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { NextPage } from 'next'
+import Page from '@layouts/Page'
+import SEO from '@components/SEO'
+
+const About: NextPage = () => {
+  return (
+    <Page>
+      <SEO title='About' />
+      <h1 className='text-3xl font-semibold dark:text-zinc-300'>This is <span className='text-blue-500'>The Best</span> blog</h1>
+      <p className='dark:text-zinc-400 mt-2'>Everything you see here is the best. And will be the best in future too 😎</p>
+    </Page>
+  )
+}
+
+export default About
