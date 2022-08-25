1910211f94d7b99ca5e3e0b4702bdb6cd040f5b5
shahriyardx <mdshahriyaralam552@gmail.com>
2022-08-26 00:27:33 +0600

    Cors enabled on the id route

--- next.config.js
+++ next.config.js
@@ -2,14 +2,6 @@
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: false,
-  async headers() {
-    return [
-      {
-        source: "*",
-        headers: [{ key: "Access-Control-Allow-Origin", value: "*" }],
-      },
-    ]
-  },
 }
 
 module.exports = nextConfig
--- pages/api/cc/[id].ts
+++ pages/api/cc/[id].ts
@@ -1,10 +1,32 @@
+import Cors from "cors"
 import { prisma } from "src/server/db/client"
 import { NextApiRequest, NextApiResponse } from "next"
+
+function initMiddleware(middleware: any) {
+  return (req: NextApiRequest, res: NextApiResponse) =>
+    new Promise((resolve, reject) => {
+      middleware(req, res, (result: any) => {
+        if (result instanceof Error) {
+          return reject(result)
+        }
+        return resolve(result)
+      })
+    })
+}
+
+const cors = initMiddleware(
+  Cors({
+    origin: "*",
+    methods: ["GET", "OPTIONS"],
+  })
+)
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  await cors(req, res)
+
   const post_id = req.query.id
 
   const post = await prisma.post.findUnique({
