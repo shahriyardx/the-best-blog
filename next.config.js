3e21c48cb2583c7d436f773e34e16a205f5acd6d
shahriyardx <mdshahriyaralam552@gmail.com>
2022-08-26 00:20:59 +0600

    Fix headers

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: false,
+  async headers() {
+    return [
+      {
+        source: "/_next/:path*",
+        headers: [{ key: "Access-Control-Allow-Origin", value: "*" }],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
